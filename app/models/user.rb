a83a8657bb3ddac0335ae65358f6be279228f874
lastsamurai0309 <61785651+lastsamurai0309@users.noreply.github.com>
2020-08-01 16:27:40 +0900

    SNS修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+         :omniauthable, omniauth_providers: %i[facebook, google_oauth2]
   validates :nickname, :last_name, :first_name, :last_name_fri, :first_name_fri, :birth_year, :birth_month, :birth_day, presence: true
   validates :password, presence: true, length: { minimum: 7 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,9 +12,9 @@ class User < ApplicationRecord
   has_one :card, dependent: :destroy
   has_many :products
   has_many :sns_credentials, dependent: :destroy
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable,
+  #        :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   def self.without_sns_data(auth)
     user = User.where(email: auth.info.email).first
